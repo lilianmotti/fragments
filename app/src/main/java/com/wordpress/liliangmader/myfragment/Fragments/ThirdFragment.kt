8c3cae55dc7150d8d5235c759b933e9a6ce5918a
LiGMA <lilianmotti@users.noreply.github.com>
2018-12-03 17:51:07 +0000

    add button next

---
+++ app/src/main/java/com/wordpress/liliangmader/myfragment/Fragments/ThirdFragment.kt
@@ -0,0 +1,61 @@
+package com.wordpress.liliangmader.myfragment.Fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.wordpress.liliangmader.myfragment.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [ThirdFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [ThirdFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class ThirdFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            ThirdFragment().apply {
+                arguments = Bundle().apply {
+                    //todo
+                }
+            }
+    }
+}
